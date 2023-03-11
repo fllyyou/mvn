d013b419fdfd9a2edf1ee45d9352360effbbc8ae
RENREN <RENREN@163.com>
2023-03-12 01:40:53 +0800

    feat 增加启动脚本

--- start.sh
+++ start.sh
@@ -10,6 +10,8 @@ if [ -z "$PID" ]; then
   else
     kill -9 "$PID"
     echo "杀死进程 - $PID"
+    sleep 3
+    rm -rf /tmp/hsperfdata_jenkins/"$PID"
     nohup  /usr/bin/java -jar target/mvn-0.0.2-SNAPSHOT.jar  &>log.out &
     jps
 fi
