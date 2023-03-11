ae2d74d795f808c7ba1fbb93b2daff255f2b573d
RENREN <RENREN@163.com>
2023-03-12 00:52:22 +0800

    feat 增加启动脚本

--- start.sh
+++ start.sh
@@ -4,9 +4,11 @@ PID=$(ps -ef | grep 'mvn-.*.jar' | grep -v grep | awk '{print $1}')
 if [ -z "$PID" ]; then
      echo "无相关进程"
      nohup  /usr/bin/java -jar target/mvn-0.0.2-SNAPSHOT.jar  &>log.out &
+     jps
   else
     kill -9 "$PID"
     echo "杀死进程 - $PID"
     nohup  /usr/bin/java -jar target/mvn-0.0.2-SNAPSHOT.jar  &>log.out &
+    jps
 fi
 
