48e3ec6c8c632ceb0da97edb1d56377d628a4ae8
RENREN <RENREN@163.com>
2023-03-12 00:12:07 +0800

    feat 增加启动脚本

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 PID=$(jps | grep 'war' | awk '{print $1}')
 if [ -z "$PID" ]; then
-     nohup java -jar ./target/mvn-0.0.2-SNAPSHOT.jar  &>log.out &
+     nohup java -jar target/mvn-0.0.2-SNAPSHOT.jar  &>log.out &
   else
     kill -9 "$PID"
-    nohup java -jar ./target/mvn-0.0.2-SNAPSHOT.jar  &>log.out &
+    nohup java -jar target/mvn-0.0.2-SNAPSHOT.jar  &>log.out &
 fi
 
