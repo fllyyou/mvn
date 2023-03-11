147d563c68e1acbf7da76f3e6ff310198601521b
RENREN <RENREN@163.com>
2023-03-12 01:32:40 +0800

    feat 增加启动脚本

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 # shellcheck disable=SC2009
 # shellcheck disable=SC2034
 #JENKINS_NODE_COOKIE='dontkillme'
-PID=$(jps | grep 'mvn-.*.jar' | awk '{print $1}')
+PID=$(ps -ef | grep 'mvn-.*.jar' | grep -v grep | awk '{print $2}')
 if [ -z "$PID" ]; then
      echo "无相关进程"
      nohup  /usr/bin/java -jar target/mvn-0.0.2-SNAPSHOT.jar  &>log.out &
