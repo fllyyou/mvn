36165ae5063227508491c09e2cb108d8339da4ad
RENREN <RENREN@163.com>
2023-03-12 00:20:28 +0800

    feat 增加启动脚本

--- start.sh
+++ start.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 PID=$(jps | grep 'war' | awk '{print $1}')
 if [ -z "$PID" ]; then
+     echo "无相关进程"
      nohup java -jar target/mvn-0.0.2-SNAPSHOT.jar  &>log.out &
   else
     kill -9 "$PID"
+    echo "杀死进程 - $PID"
     nohup java -jar target/mvn-0.0.2-SNAPSHOT.jar  &>log.out &
 fi
 
